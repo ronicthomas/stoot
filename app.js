6194431b571e942920e1882b1b43d0c316060370
Roni C Thomas <roni@intelligrape.com>
2013-04-15 02:33:37 +0530

    Commited app.js :P

---
+++ app.js
@@ -0,0 +1,42 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+        , routes = require('./routes')
+        , http = require('http')
+        , path = require('path')
+        , channelService = require('./services/channelService')
+        , io = require('socket.io')
+        , SERVER_CONFIG = require('config').SERVER
+        , server;
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+server = http.createServer(app);
+io = io.listen(server).set("log level", 2);
+channelService.setup(io);
+
+app.get("/", routes.index);
+
+server.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
