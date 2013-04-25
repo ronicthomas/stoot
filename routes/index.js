c811845c5868bf3ec8c9f002db2559f8ff8401de
Roni C Thomas <roni@intelligrape.com>
2013-04-25 18:15:28 +0530

    Removed session identifier code :P

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,14 @@
 exports.index = function (req, res) {
-    res.render("index", {title:"Stoot", uuid: require('node-uuid')()});
+    res.render('index', {})
+};
+
+exports.dashboard = function (req, res) {
+    res.render("dashboard", {});
+};
+
+exports.authenticate = function(req ,res) {
+    console.log(req.param);
+    var username = req.param('username');
+    var password = req.param('password');
+    console.log([username, password])
 };
