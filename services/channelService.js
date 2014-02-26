6aedfebd8e44c6206a887f6cfb85bf66acbf497b
Roni C Thomas <roni@intelligrape.com>
2014-02-26 17:47:04 +0530

    Removed all transports

--- services/channelService.js
+++ services/channelService.js
@@ -1,11 +1,7 @@
 exports.setup = function (io) {
     io.configure("production", function () {
         io.set("transports", [
-            'websocket'
-            , 'flashsocket'
-            , 'htmlfile'
-            , 'xhr-polling'
-            , 'jsonp-polling'
+            'xhr-polling'
         ]);
         io.set("polling duration", 20);
     });
